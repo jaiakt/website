b53eb56a87a4b3244b0e84a3e32025482ec5ef2d
Jai Srivastav <jaiakt@yahoo.com>
2014-04-19 19:14:08 -0700

    make navigation bar sticky and added banner

--- js/script.js
+++ js/script.js
@@ -5,7 +5,24 @@ function highlightCorrectNavBarElement() {
 }
 */
 
+var changedText = new Array("my name is Jai Srivastav", "I am an app developer", "I am a web developer");
+var changeTextIndex = 0;
+
+function changeText() {
+	var fadeInTime = 1000;
+	var delayTime = 1250;
+	var fadeOutTime = 1000;
+	var totalTime = fadeInTime + delayTime + fadeOutTime;
+	$(".changingText").text(changedText[changeTextIndex]);
+	$(".changingText").fadeTo(fadeInTime, 1).delay(delayTime).fadeTo(fadeOutTime, 0, function() {
+		changeTextIndex = (changeTextIndex + 1) % changedText.length;
+		changeText();
+	});
+}
+
 $(document).ready(function(){
+
+	//Navigation Bar Link Fade In/Out
 	$("li.navBarLink").hover(
 		function() {
 			console.log("hovered");
@@ -17,7 +34,20 @@ $(document).ready(function(){
 				backgroundColor: "black"
 			}, 150);
 		});
+
+	//Navigation Bar Sticks top 
+	var stickyNavTop = $("header").offset().top;
 	$(document).scroll(function(){
-		
+		if ($(window).scrollTop() >= stickyNavTop) {
+			$("header").addClass("stickyNavBar");
+			$("#about").addClass("stickyAbout");
+		}
+		else {
+			$("header").removeClass("stickyNavBar");
+			$("#about").removeClass("stickyAbout");
+		}
 	});
+
+	changeText();
+
 });
